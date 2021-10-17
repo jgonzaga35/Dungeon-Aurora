d618b5bd36860c3714f382e281723a0ebe2fb3a8
Mathieu PATUREL <australie.p@gmail.com>
2021-10-17 17:15:52 +1100

    test loading maze

---
+++ src/test/java/dungeonmania/playerMovementTest.java
@@ -0,0 +1,46 @@
+package dungeonmania;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Objects;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.response.models.EntityResponse;
+
+public class playerMovementTest {
+
+    @ParameterizedTest
+    @ValueSource(strings={"Peaceful", "Standard", "Hard"}) // test for all modes
+    public void testLoadingMaze(String mode) {
+        DungeonManiaController ctr = new DungeonManiaController();
+
+        DungeonResponse resp = assertDoesNotThrow(() -> {
+            return ctr.newGame("maze", mode);
+        });
+
+        assertEquals("maze", resp.getDungeonName());
+        assertEquals(0, resp.getInventory().size());
+        int numPlayers = 0;
+        int numExit = 0;
+        int numWalls = 0;
+
+        for (EntityResponse er : resp.getEntities()) {
+            if (Objects.equals(er.getType(), "wall")) {
+                numWalls++;
+            } else if (Objects.equals(er.getType(), "player")) {
+                numPlayers++;
+            } else if (Objects.equals(er.getType(), "exit")) {
+                numExit++;
+            }
+        }
+
+        assertEquals(1, numExit);
+        assertEquals(1, numPlayers);
+        // cat src/test/resources/dungeons/maze.json | grep wall | wc -l
+        assertEquals(204, numWalls);
+    }
+}
