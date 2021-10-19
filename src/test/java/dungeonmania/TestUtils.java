b3147b5783269fec4eb3b03bf85257bbe321f4f2
Mathieu PATUREL <australie.p@gmail.com>
2021-10-19 14:13:19 +1100

    zombie spawner: test when it's blocked with either border or walls

--- src/test/java/dungeonmania/TestUtils.java
+++ src/test/java/dungeonmania/TestUtils.java
@@ -1,5 +1,9 @@
 package dungeonmania;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
 import dungeonmania.response.models.DungeonResponse;
@@ -14,5 +18,17 @@ public class TestUtils {
             }
         }
         throw new Error("player wasn't found");
+    }
+
+    /**
+     * asserts that as and bs contain the same element
+     * @param <T>
+     * @param as
+     * @param bs
+     */
+    public static <T extends Comparable<T>> void assertEqualsUnordered(List<T> as, List<T> bs) {
+        Collections.sort(as);
+        Collections.sort(bs);
+        assertEquals(as, bs);
     }
 }
