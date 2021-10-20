ceb2d3c84352f5eeb0ee27ba20d6273dcf3d3e4a
CarelGreylin <z5235774@ad.unsw.edu.au>
2021-10-21 10:42:41 +1100

    Started exit tests.

---
+++ src/test/java/dungeonmania/entities/statics/TestExit.java
@@ -0,0 +1,55 @@
+package dungeonmania.entities.statics;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import dungeonmania.DungeonManiaController;
+import dungeonmania.TestUtils;
+import dungeonmania.DungeonManiaController.GameMode;
+import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.util.Direction;
+import dungeonmania.util.Position;
+
+public class TestExit {
+    @Test
+    public void testPlayerMovement() {
+        DungeonManiaController ctr = new DungeonManiaController();
+        DungeonResponse resp;
+        Position p;
+        assertDoesNotThrow(() -> {
+            ctr.newGame("_simple", GameMode.PEACEFUL.getValue());
+        });
+
+        // don't move
+        resp = ctr.tick("", Direction.NONE);
+        p = TestUtils.getPlayerPosition(resp);
+        assertEquals(0, p.getX());
+        assertEquals(0, p.getY());
+
+        // move on a free spot
+        resp = ctr.tick("", Direction.RIGHT);
+        p = TestUtils.getPlayerPosition(resp);
+        assertEquals(1, p.getX());
+        assertEquals(0, p.getY());
+
+        // try to move against a wall
+        resp  = ctr.tick("", Direction.DOWN);
+        p = TestUtils.getPlayerPosition(resp);
+        assertEquals(1, p.getX());
+        assertEquals(0, p.getY());
+
+        // try to move out of the map
+        resp  = ctr.tick("", Direction.UP);
+        p = TestUtils.getPlayerPosition(resp);
+        assertEquals(1, p.getX());
+        assertEquals(0, p.getY());
+
+        // move back to starting point
+        resp  = ctr.tick("", Direction.LEFT);
+        p = TestUtils.getPlayerPosition(resp);
+        assertEquals(0, p.getX());
+        assertEquals(0, p.getY());
+    }
+}
