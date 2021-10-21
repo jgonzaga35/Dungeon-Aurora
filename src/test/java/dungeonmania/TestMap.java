e81105d040253ed11860dc5de0e8b880fbb478e0
CarelGreylin <z5235774@ad.unsw.edu.au>
2021-10-21 16:26:36 +1100

    Map floodfill test.

--- src/test/java/dungeonmania/TestMap.java
+++ src/test/java/dungeonmania/TestMap.java
@@ -1,6 +1,7 @@
 package dungeonmania;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
 
@@ -20,8 +21,20 @@ public class TestMap {
 
             System.out.println(dungeon.getMap().toString());
         });
+    }
 
+    @Test
+    public void testMapFlood() throws IOException {
+        String content = FileLoader.loadResourceFile("/dungeons/_simple.json");
+        Dungeon dungeon = Dungeon.fromJSONObject("name", GameMode.STANDARD, new JSONObject(content));
 
+        dungeon.getMap().flood();
 
+        System.out.println(dungeon.getMap().toString());
+
+        assertEquals(1, dungeon.getMap().getCell(1, 0).getPlayerDistance());
+        assertEquals(2, dungeon.getMap().getCell(2, 0).getPlayerDistance());
+        assertEquals(3, dungeon.getMap().getCell(3, 0).getPlayerDistance());
+        assertEquals(4, dungeon.getMap().getCell(4, 0).getPlayerDistance());
     }
 }
