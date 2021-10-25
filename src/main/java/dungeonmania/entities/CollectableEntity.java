b1b129a33c91bd0c6cdacc8bea602f24c62799ed
Ethan Marlow <z5312704@ad.unsw.edu.au>
2021-10-25 17:05:03 +1100

    Implementation v1.0 - Creating Basic Entity Structures

---
+++ src/main/java/dungeonmania/entities/CollectableEntity.java
@@ -0,0 +1,42 @@
+package dungeonmania.entities;
+
+import dungeonmania.Cell;
+import dungeonmania.Dungeon;
+import dungeonmania.DungeonManiaController.LayerLevel;
+import dungeonmania.util.Direction;
+import dungeonmania.Entity;
+import dungeonmania.Pos2d;
+
+public abstract class CollectableEntity extends Entity {
+
+    public CollectableEntity(Dungeon dungeon, Pos2d position) {
+        super(dungeon, position);
+    }
+
+    /**
+     * Moves an entity from the current cell to the target cell
+     * @param target
+     */
+    public void moveTo(Cell target) {
+        Cell from = this.getCell();
+        from.removeOccupant(this);
+        target.addOccupant(this);
+
+        this.position = target.getPosition();
+        this.getCell().onWalked(from.getPosition(), this.position);
+    }
+
+    public Cell inspectCell(Direction d) {
+        return dungeon.getMap().getCellAround(dungeon.getMap().getCell(position), d);
+    }
+
+    @Override
+    public boolean isInteractable() {
+        return false; // i think at least
+    }
+
+    @Override
+    public LayerLevel getLayerLevel() {
+        return LayerLevel.MOVING_ENTITY;
+    }
+}
---
+++ src/main/java/dungeonmania/entities/collectables/Treasure.java
@@ -0,0 +1,29 @@
+// java doesn't support static folders, since it's a folder, so we use the plural
+package dungeonmania.entities.collectables;
+
+import dungeonmania.Dungeon;
+import dungeonmania.Pos2d;
+import dungeonmania.entities.CollectableEntity;
+
+public class Treasure extends CollectableEntity {
+
+    public static String STRING_TYPE = "treasure";
+
+    public Treasure(Dungeon dungeon, Pos2d position) {
+        super(dungeon, position);
+    }
+
+    @Override
+    public String getTypeAsString() {
+        return Treasure.STRING_TYPE;
+    }
+
+    @Override
+    public boolean isInteractable() {
+        return false; // i don't think so at least
+    }
+
+    @Override
+    public void tick() {
+    }
+}
