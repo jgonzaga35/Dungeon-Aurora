8b67258d05893e1c59e782ef125acc566157a858
Ethan Marlow <z5312704@ad.unsw.edu.au>
2021-10-25 16:57:42 +1100

    Treasure Testing Version 3.0

--- src/test/java/dungeonmania/TestCollectables.java
+++ src/test/java/dungeonmania/TestCollectables.java
@@ -9,6 +9,8 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Stream;
 
+import org.json.JSONObject;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -16,7 +18,8 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import dungeonmania.DungeonManiaController.GameMode;
 import dungeonmania.response.models.DungeonResponse;
-import dungeonmania.response.models.EntityResponse;
+import dungeonmania.util.Direction;
+import dungeonmania.util.FileLoader;
 
 public class TestCollectables {
     /** 
@@ -25,25 +28,39 @@ public class TestCollectables {
     
     @Test
     public void testLoadingTreasure() {
-        //Loading New Map (Treasure is at Coords (7, 10))
-        String content = FileLoader.loadResourceFile("/dungeons/advanced.json");
-        Dungeon dungeon = Dungeon.fromJSONObject("name", GameMode.STANDARD, new JSONObject(content));
+        assertDoesNotThrow(() -> {
+            //Loading New Map (Treasure is at Coords (7, 10) and Player is at Coords (1, 1))
+            String content = FileLoader.loadResourceFile("/dungeons/treasureExample.json");
+            Dungeon dungeon = Dungeon.fromJSONObject("name", GameMode.STANDARD, new JSONObject(content));
+        });
 
         //New Game
         DungeonManiaController ctr = new DungeonManiaController();
         DungeonResponse resp = ctr.newGame("maze", GameMode.PEACEFUL.getValue());
         resp = ctr.tick("", Direction.NONE);
 
-        //Checking if Character Has Any Treasure (Should be None)
-
-
         //Moving to the Treasure
 
-        // TODO
-        resp  = ctr.tick("", Direction.DOWN);
+        // Down 6 Units to Coord (7, 1)
+        ctr.tick("", Direction.DOWN);
+        ctr.tick("", Direction.DOWN);
+        ctr.tick("", Direction.DOWN);
+        ctr.tick("", Direction.DOWN);
+        ctr.tick("", Direction.DOWN);
+        ctr.tick("", Direction.DOWN);
+
+        // Right 9 Units to Coord (7, 1)
+        ctr.tick("", Direction.RIGHT);
+        ctr.tick("", Direction.RIGHT);
+        ctr.tick("", Direction.RIGHT);
+        ctr.tick("", Direction.RIGHT);
+        ctr.tick("", Direction.RIGHT);
+        ctr.tick("", Direction.RIGHT);
+        ctr.tick("", Direction.RIGHT);
+        ctr.tick("", Direction.RIGHT);
+        ctr.tick("", Direction.RIGHT);
         
         //Checking If Goal is Reached (No Treasure Left on Map)
-
         //TODO
     }
 
