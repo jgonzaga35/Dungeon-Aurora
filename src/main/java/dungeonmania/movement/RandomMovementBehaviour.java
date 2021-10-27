584be85b2de59275634346d60d2603b68fa76f20
CarelGreylin <z5235774@ad.unsw.edu.au>
2021-10-27 11:33:47 +1100

    Implemented Random Movement behaviour.

--- src/main/java/dungeonmania/movement/RandomMovementBehaviour.java
+++ src/main/java/dungeonmania/movement/RandomMovementBehaviour.java
@@ -1,6 +1,9 @@
 package dungeonmania.movement;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
@@ -10,6 +13,12 @@ import dungeonmania.util.Direction;
 
 public class RandomMovementBehaviour implements Movement {
     private Random rng = new Random();
+    private List<Direction> directions = new ArrayList<Direction>(Arrays.asList(
+        Direction.LEFT,
+        Direction.RIGHT,
+        Direction.UP,
+        Direction.DOWN
+    ));
     private Map<Integer, Direction> directionMap = new HashMap<Integer, Direction>();
     private DungeonMap map;
     private Cell currentCell;
@@ -26,9 +35,48 @@ public class RandomMovementBehaviour implements Movement {
 
     public Cell move()
     {
-        int direction = rng.nextInt(4);
+        List<Direction> modifiedDirections = new ArrayList<Direction>(directions);
+        
+        // Remove invalid movement directions
+        if (currentCell.getPosition().getX() == 0)
+        {
+            modifiedDirections.remove(Direction.LEFT);
+        }
+        else if (map.getCellAround(currentCell, Direction.LEFT).isBlocking())
+        {
+            modifiedDirections.remove(Direction.LEFT);
+        }
 
-        currentCell = map.getCellAround(currentCell, directionMap.get(direction));
+        if (currentCell.getPosition().getX() == map.getWidth() - 1)
+        {
+            modifiedDirections.remove(Direction.RIGHT);
+        }
+        else if (map.getCellAround(currentCell, Direction.RIGHT).isBlocking())
+        {
+            modifiedDirections.remove(Direction.RIGHT);
+        }
+
+        if (currentCell.getPosition().getY() == 0)
+        {
+            modifiedDirections.remove(Direction.UP);
+        }
+        else if (map.getCellAround(currentCell, Direction.UP).isBlocking())
+        {
+            modifiedDirections.remove(Direction.UP);
+        }
+
+        if (currentCell.getPosition().getY() == map.getHeight() - 1)
+        {
+            modifiedDirections.remove(Direction.DOWN);
+        }
+        else if (map.getCellAround(currentCell, Direction.DOWN).isBlocking())
+        {
+            modifiedDirections.remove(Direction.DOWN);
+        }
+
+        int direction = rng.nextInt(modifiedDirections.size());
+        
+        currentCell = map.getCellAround(currentCell, modifiedDirections.get(direction));
 
         return currentCell;
     }
