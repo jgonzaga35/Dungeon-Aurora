82463bfcfdfb8e1dc9470a4c1a12b359679d79dd
CarelGreylin <z5235774@ad.unsw.edu.au>
2021-10-30 19:37:39 +1100

    Redid the implementation of Invincibility potion.

--- src/main/java/dungeonmania/entities/collectables/consumables/InvincibilityPotion.java
+++ src/main/java/dungeonmania/entities/collectables/consumables/InvincibilityPotion.java
@@ -1,7 +1,11 @@
 package dungeonmania.entities.collectables.consumables;
 
 import dungeonmania.Dungeon;
+import dungeonmania.Entity;
 import dungeonmania.Pos2d;
+import dungeonmania.entities.MovingEntity;
+import dungeonmania.entities.movings.Player;
+import dungeonmania.movement.FleeMovementBehaviour;
 
 public class InvincibilityPotion extends Potion {
 
@@ -9,6 +13,20 @@ public class InvincibilityPotion extends Potion {
 
     public InvincibilityPotion(Dungeon dungeon, Pos2d position) {
         super(dungeon, position);
+        duration = 5;
+    }
+
+    @Override
+    public void use() {
+        dungeon.getOccupants().stream()
+        .filter(e -> e instanceof MovingEntity)
+        .filter(e -> !(e instanceof Player))
+        .forEach(e -> {
+            MovingEntity enemy = (MovingEntity) e;
+            enemy.addMovementBehaviour(
+                new FleeMovementBehaviour(2, dungeon.getMap(), enemy.getCell())
+            );
+        });
     }
 
     @Override
