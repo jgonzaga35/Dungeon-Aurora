ef0887fabf02073646ecccd2634f870e39283d33
CarelGreylin <z5235774@ad.unsw.edu.au>
2021-10-30 19:36:11 +1100

    Working on testing consumables.

--- src/test/java/dungeonmania/TestConsumables.java
+++ src/test/java/dungeonmania/TestConsumables.java
@@ -1,6 +1,7 @@
 package dungeonmania;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -13,17 +14,20 @@ import org.junit.jupiter.api.Test;
 import dungeonmania.DungeonManiaController.GameMode;
 import dungeonmania.entities.collectables.ConsumableEntity;
 import dungeonmania.entities.collectables.consumables.InvincibilityPotion;
+import dungeonmania.entities.movings.ZombieToast;
+import dungeonmania.util.Direction;
 import dungeonmania.util.FileLoader;
 
 public class TestConsumables {
+    DungeonManiaController dc;
     Dungeon dungeon;
-    ConsumableEntity consumable;
 
     @BeforeEach
     public void setStartingPostition() throws IOException {
-        String content = FileLoader.loadResourceFile("/dungeons/_simple.json");
+        String content = FileLoader.loadResourceFile("/dungeons/_test_potions.json");
         dungeon = Dungeon.fromJSONObject("name", GameMode.STANDARD, new JSONObject(content));
         dungeon.getMap().flood();
+        dc = new DungeonManiaController(dungeon);
     }
 
     @Test
@@ -31,10 +35,31 @@ public class TestConsumables {
         InvincibilityPotion potion = new InvincibilityPotion(dungeon, new Pos2d(3, 2));
         List<ConsumableEntity> testList = new ArrayList<>();
         potion.onItemUse(n -> testList.add(n));
-
+        
         potion.use();
         potion.use();
         
         assertEquals(2, testList.size());
+    }
+    
+    @Test
+    public void testInvincibilityFleeBehaviour() {
+        Cell zomCell = dungeon.getMap().getCell(5, 9);
+        ZombieToast zombie = new ZombieToast(dungeon, zomCell.getPosition());
+        zomCell.addOccupant(zombie);
+
+        dc.tick(null, Direction.UP);
+        dc.tick(null, Direction.DOWN);
+        
+
+        Integer zomDist = zombie.getCell().getPlayerDistance();
+        dc.tick("invincibility_potion", Direction.NONE);
+        for (int i = 0; i < 5; i++) {
+            assertTrue(zomDist < zombie.getCell().getPlayerDistance());
+            zomDist = zombie.getCell().getPlayerDistance();
+            dc.tick(null, Direction.NONE);
+        }
+        
+
     }
 }
