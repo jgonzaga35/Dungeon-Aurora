93feb85caf1254d7435534d216a4f519f4adec0b
CarelGreylin <z5235774@ad.unsw.edu.au>
2021-10-31 13:56:37 +1100

    Created friendly Movement behaviour.

---
+++ src/main/java/dungeonmania/movement/FriendlyMovementBehaviour.java
@@ -0,0 +1,41 @@
+package dungeonmania.movement;
+
+import java.util.stream.Stream;
+
+import dungeonmania.Cell;
+import dungeonmania.DungeonMap;
+
+public class FriendlyMovementBehaviour extends MovementBehaviour {
+    private DungeonMap map;
+    private Cell currentCell;
+
+    public FriendlyMovementBehaviour(int precedence, DungeonMap map, Cell initialCell) {
+        super(precedence);
+        this.map = map;
+        this.currentCell = initialCell;
+    }
+
+    @Override
+    public Cell move() {
+        Stream<Cell> neighbors = map.getNeighbors(currentCell).stream();
+        if (neighbors.anyMatch(c -> c.getPlayerDistance() == 1)) {
+            // Stay one away if possible.
+            currentCell = neighbors.filter(c -> c.getPlayerDistance() == 1)
+                .findAny().get();
+        } else {
+            // else get as close as possible
+            currentCell = neighbors.min(
+                    (c1, c2) -> 
+                    Integer.compare(c1.getPlayerDistance(), c2.getPlayerDistance())
+                ).get();
+        }
+        
+        return currentCell;
+    }
+
+    @Override
+    public Cell getCurrentCell() {
+        return currentCell;
+    }
+    
+}
