02272bf4d5c185989f1e2991a4ea4589a79a8a6f
CarelGreylin <z5235774@ad.unsw.edu.au>
2021-11-01 01:00:04 +1100

    Fixed incorrect tests and not ticking between assertions.

--- src/test/java/dungeonmania/TestPotions.java
+++ src/test/java/dungeonmania/TestPotions.java
@@ -1,5 +1,6 @@
 package dungeonmania;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
@@ -11,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import dungeonmania.DungeonManiaController.GameMode;
 import dungeonmania.entities.collectables.consumables.InvincibilityPotion;
 import dungeonmania.entities.movings.Mercenary;
+import dungeonmania.entities.movings.Spider;
 import dungeonmania.entities.movings.ZombieToast;
 import dungeonmania.response.models.DungeonResponse;
 import dungeonmania.util.Direction;
@@ -100,9 +102,74 @@ public class TestPotions {
             mercDist = merc.getCell().getPlayerDistance();
         }
     }
+    
+    @Test
+    public void testNotUsingPotion() {        
+        dc.tick(null, Direction.UP);
+        
+        Cell mercCell = dungeon.getMap().getCell(8, 8);
+        Mercenary merc = new Mercenary(dungeon, mercCell.getPosition());
+        mercCell.addOccupant(merc);
+        
+        Integer mercDist = merc.getCell().getPlayerDistance();
+        for (int i = 0; i < 4; i++) {
+            dc.tick(null, Direction.NONE);
+            assertTrue(mercDist > merc.getCell().getPlayerDistance());
+            mercDist = merc.getCell().getPlayerDistance();
+        }
+    }
 
     @Test
     public void testInvincibilityOnSpiders() {
-
+        Cell spiderCell = dungeon.getMap().getCell(5, 6);
+        Spider spider = new Spider(dungeon, spiderCell.getPosition());
+        spiderCell.addOccupant(spider);
+        
+        dc.tick(null, Direction.UP);
+        // Spider moves up into loop, next step would have been to go right.
+        
+        Integer spiderDist = spider.getCell().getPlayerDistance();
+        
+        // use pot
+        dc.tick(invincibilityPot.getId(), Direction.NONE);
+        
+        for (int i = 0; i < 4; i++) {
+            assertTrue(spiderDist < spider.getCell().getPlayerDistance());
+            spiderDist = spider.getCell().getPlayerDistance();
+            dc.tick(null, Direction.NONE);
+        }
+        assertTrue(
+            spider.getCell().getPosition().equals(new Pos2d(8, 7)) ||
+            spider.getCell().getPosition().equals(new Pos2d(7, 8)) ||
+            spider.getCell().getPosition().equals(new Pos2d(0, 7)) ||
+            spider.getCell().getPosition().equals(new Pos2d(3, 8))
+        );
+        
+        // spider should now continue into the loop by moving right or bouncing back.
+        dc.tick(null, Direction.NONE);
+        assertTrue(
+            spider.getCell().getPosition().equals(new Pos2d(8, 7)) || // bounce
+            spider.getCell().getPosition().equals(new Pos2d(8, 8)) || // right
+            spider.getCell().getPosition().equals(new Pos2d(1, 7)) || // right
+            spider.getCell().getPosition().equals(new Pos2d(4, 8))    // right
+            );
+            
+        dc.tick(null, Direction.NONE);
+        // then down or left
+        assertTrue(
+            spider.getCell().getPosition().equals(new Pos2d(7, 7)) || // left
+            spider.getCell().getPosition().equals(new Pos2d(8, 8)) || // bounce
+            spider.getCell().getPosition().equals(new Pos2d(1, 8)) || // down
+            spider.getCell().getPosition().equals(new Pos2d(4, 8))    // bounce
+            );
+                
+                
+        dc.tick(null, Direction.NONE);
+        assertTrue(
+            spider.getCell().getPosition().equals(new Pos2d(6, 7)) || // left
+            spider.getCell().getPosition().equals(new Pos2d(7, 8)) || // left
+            spider.getCell().getPosition().equals(new Pos2d(1, 8)) || // bounce
+            spider.getCell().getPosition().equals(new Pos2d(3, 8))    // left
+        );
     }
 }
