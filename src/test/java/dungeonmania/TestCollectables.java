6c651c83f6b0bb7f76184992ed01c73d549bad67
Ethan Marlow <z5312704@ad.unsw.edu.au>
2021-11-01 10:36:04 +1100

    The One Ring Testing

--- src/test/java/dungeonmania/TestCollectables.java
+++ src/test/java/dungeonmania/TestCollectables.java
@@ -311,4 +311,72 @@ public class TestCollectables {
         //Check that Player has Lost Battle
         //TODO
     }
+
+    /** 
+     * TEST: Ensure The One Ring is Collected  
+     */
+    
+    @Test
+    public void testLoadingTheOneRing() {
+        //New Game
+        DungeonManiaController ctr = new DungeonManiaController();
+        DungeonResponse resp = ctr.newGame("_theOneRingExample", GameMode.PEACEFUL.getValue());
+        resp = ctr.tick(null, Direction.NONE);
+        Boolean found = false;
+
+        // Down 2 Units to The One Ring at Coord (1, 3)
+        ctr.tick(null, Direction.DOWN);
+        resp = ctr.tick(null, Direction.DOWN);
+
+        //Checking If The One Ring was Collected
+        found = false;
+        String curr_type = "";
+        String ringId = "";
+        List<ItemResponse> curr_inventory = resp.getInventory();
+        for (ItemResponse item : curr_inventory) {
+            curr_type = item.getType();
+            if (curr_type == "one_ring") {
+                found = true;
+                ringId = item.getId(); 
+            }
+        }
+        assertEquals(true, found);
+
+        //Check that the Item Was Removed from the Cell
+        int currPositionX = 0;
+        int currPositionY = 0;
+        boolean itemRemoved = true;
+        List<EntityResponse> cellEntities = resp.getEntities();
+        for (EntityResponse currEntity : cellEntities) {
+            curr_type = currEntity.getType();
+
+            Position currPosition = currEntity.getPosition();
+            currPositionX = currPosition.getX();
+            currPositionY = currPosition.getY();
+            if (curr_type == "one_ring" && currPositionX == 1 && currPositionY == 3) {
+                itemRemoved = false;
+            }
+        }
+        assertEquals(true, itemRemoved);
+
+        //Use The One Ring 
+        resp = ctr.tick(ringId, Direction.RIGHT);
+
+        //Check it is Removed from Inventory
+        itemRemoved = true;
+        String curr_id = "";
+        List<ItemResponse> inventory = resp.getInventory();
+        for (ItemResponse currItem : inventory) {
+            curr_type = currItem.getType();
+            curr_id = currItem.getId();
+
+            if ((curr_type == "one_ring") && (curr_id.equals(ringId) == true)) {
+                itemRemoved = false;
+            }
+        }
+        assertEquals(true, itemRemoved);
+
+        //Check that Health is Boosted to 100
+        //TODO
+    }
 }
