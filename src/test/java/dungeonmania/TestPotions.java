540b644d16bc10f8e34725a501aeeff9bf5ea27b
CarelGreylin <z5235774@ad.unsw.edu.au>
2021-10-31 16:48:31 +1100

    Test for Invincibility potion.

--- src/test/java/dungeonmania/TestPotions.java
+++ src/test/java/dungeonmania/TestPotions.java
@@ -9,40 +9,49 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import dungeonmania.DungeonManiaController.GameMode;
+import dungeonmania.entities.collectables.consumables.InvincibilityPotion;
 import dungeonmania.entities.movings.ZombieToast;
+import dungeonmania.response.models.DungeonResponse;
 import dungeonmania.util.Direction;
 import dungeonmania.util.FileLoader;
 
 public class TestPotions {
     DungeonManiaController dc;
     Dungeon dungeon;
+    InvincibilityPotion invinsiblePot;
 
     @BeforeEach
     public void setStartingPostition() throws IOException {
         String content = FileLoader.loadResourceFile("/dungeons/_test_potions.json");
         dungeon = Dungeon.fromJSONObject("name", GameMode.STANDARD, new JSONObject(content));
         dungeon.getMap().flood();
+
+        
+        Cell invinsibleCell = dungeon.getMap().getCell(5, 4);
+        invinsiblePot= new InvincibilityPotion(dungeon, invinsibleCell.getPosition());
+        invinsibleCell.addOccupant(invinsiblePot);
+        
         dc = new DungeonManiaController(dungeon);
+        // player in (5, 5) with no inventory
     }
     
     @Test
     public void testInvincibilityFleeBehaviour() {
-        Cell zomCell = dungeon.getMap().getCell(5, 9);
+        Cell zomCell = dungeon.getMap().getCell(5, 7);
         ZombieToast zombie = new ZombieToast(dungeon, zomCell.getPosition());
         zomCell.addOccupant(zombie);
-
-        dc.tick(null, Direction.UP);
-        dc.tick(null, Direction.DOWN);
         
-
+        dc.tick(null, Direction.UP);
+        
         Integer zomDist = zombie.getCell().getPlayerDistance();
-        dc.tick("invincibility_potion", Direction.NONE);
+
+        // use pot
+        dc.tick(invinsiblePot.getId(), Direction.NONE);
+
         for (int i = 0; i < 5; i++) {
             assertTrue(zomDist < zombie.getCell().getPlayerDistance());
             zomDist = zombie.getCell().getPlayerDistance();
             dc.tick(null, Direction.NONE);
         }
-        
-
     }
 }
