3b72f4193a8e8195fc059aff70e9de19cfe5a012
CarelGreylin <z5235774@ad.unsw.edu.au>
2021-10-31 15:03:51 +1100

    Added the posibility to stay in the current cell.

--- src/main/java/dungeonmania/movement/FriendlyMovementBehaviour.java
+++ src/main/java/dungeonmania/movement/FriendlyMovementBehaviour.java
@@ -17,14 +17,18 @@ public class FriendlyMovementBehaviour extends MovementBehaviour {
 
     @Override
     public Cell move() {
+        // remain at the players side if possible.
+        if (currentCell.getPlayerDistance() == 1) return currentCell;
+
         Stream<Cell> neighbors = map.getNeighbors(currentCell).stream();
         if (neighbors.anyMatch(c -> c.getPlayerDistance() == 1)) {
             // Stay one away if possible.
-            currentCell = neighbors.filter(c -> c.getPlayerDistance() == 1)
+            currentCell = map.getNeighbors(currentCell).stream()
+                .filter(c -> c.getPlayerDistance() == 1)
                 .findAny().get();
         } else {
             // else get as close as possible
-            currentCell = neighbors.min(
+            currentCell = map.getNeighbors(currentCell).stream().min(
                     (c1, c2) -> 
                     Integer.compare(c1.getPlayerDistance(), c2.getPlayerDistance())
                 ).get();
