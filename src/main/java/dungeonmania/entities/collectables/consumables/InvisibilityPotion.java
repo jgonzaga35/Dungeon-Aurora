257425d2dd0c8686e506026effbbbb9434cb54c6
CarelGreylin <z5235774@ad.unsw.edu.au>
2021-11-01 11:05:14 +1100

    Invis skeleton and testing.

---
+++ src/main/java/dungeonmania/entities/collectables/consumables/InvisibilityPotion.java
@@ -0,0 +1,31 @@
+package dungeonmania.entities.collectables.consumables;
+
+import dungeonmania.Dungeon;
+import dungeonmania.Pos2d;
+
+public class InvisibilityPotion extends Potion {
+
+    public InvisibilityPotion(Dungeon dungeon, Pos2d position) {
+        super(dungeon, position);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void applyEffects() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void expire() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public String getTypeAsString() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
