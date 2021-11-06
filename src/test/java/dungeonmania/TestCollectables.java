045dd74feabf96f65558f7765b56739419723e00
Ethan Marlow <z5312704@ad.unsw.edu.au>
2021-11-06 18:30:03 +1100

    Updating Map & Tests for Testing Bomb for Activating Switches

--- src/test/java/dungeonmania/TestCollectables.java
+++ src/test/java/dungeonmania/TestCollectables.java
@@ -325,7 +325,7 @@ public class TestCollectables {
     
     @Test
     public void testLoadingBomb() {
-        //Item Coords: Player(1,1), Bomb(1,3), Switch(7,3)
+        //Item Coords: Player(1,1), Bomb(1,3), Boulder (7,2), Switch(7,3)
         //New Game
         DungeonManiaController ctr = new DungeonManiaController();
         DungeonResponse resp = ctr.newGame("_bombExample", GameMode.PEACEFUL.getValue());
@@ -385,7 +385,32 @@ public class TestCollectables {
         //Place bomb onto (5,3)
         resp = ctr.tick(bombId, Direction.NONE);
 
-        
+        //Go Up and Right to Move to (6,1)
+        ctr.tick(null, Direction.UP);
+        ctr.tick(null,Direction.UP);
+        ctr.tick(null, Direction.RIGHT);
+
+        //Move Down to (6,2) to Push Boulder Onto Switch & Explode Bomb
+        ctr.tick(null, Direction.DOWN);
+
+        //Move to (6,3) where Switch Was & Ensure There are No Entities Here
+        resp = ctr.tick(null, Direction.DOWN);
+
+        boolean entityFound = false;
+        boolean playerFound = false;
+        curr_type = "";
+        List<EntityResponse> currEntities = resp.getEntities();
+        for (EntityResponse item : currEntities) {
+            curr_type = item.getType();
+            if (curr_type.equals(Player.STRING_TYPE)) {
+                playerFound = true;
+            }
+            if ((curr_type.equals(Wall.STRING_TYPE)) || (curr_type.equals(FloorSwitch.STRING_TYPE)) || (curr_type.equals(Bomb.STRING_TYPE))) {
+                entityFound = true;
+            }
+        }
+        assertEquals(true, playerFound);
+        assertEquals(false, entityFound);
 
         //Check Entities in Blast Radius Destroyed
         //Blast Radius is 1, therefore if bomb exploads it destroys
@@ -396,10 +421,10 @@ public class TestCollectables {
         //than player
 
         //Player is at Cell (5,3) check no entities here
-        boolean entityFound = false;
-        boolean playerFound = false;
+        entityFound = false;
+        playerFound = false;
         curr_type = "";
-        List<EntityResponse> currEntities = resp.getEntities();
+        currEntities = resp.getEntities();
         for (EntityResponse item : currEntities) {
             curr_type = item.getType();
             if (curr_type.equals(Player.STRING_TYPE)) {
