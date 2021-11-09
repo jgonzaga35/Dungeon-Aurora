c539ec957886fd7d12eff42e437cdb3ec278f850
davidliu <daavidliu@gmail.com>
2021-11-10 01:21:33 +1100

    test added for sceptre

--- src/test/java/dungeonmania/TestBuildableV2.java
+++ src/test/java/dungeonmania/TestBuildableV2.java
@@ -16,32 +16,40 @@ import dungeonmania.util.*;
 
 public class TestBuildableV2 {
     @Test
+    public void testBuildSceptreInvalid() {
+        DungeonManiaController ctr = new DungeonManiaController();
+        ctr.newGame("_buildable", GameMode.PEACEFUL.getValue());
+        assertThrows(InvalidActionException.class, () -> {
+            ctr.build(Sceptre.STRING_TYPE);
+        });
+    }
+
+    @Test
     public void testBuildSceptre1() {
         DungeonManiaController ctr = new DungeonManiaController();
-        DungeonResponse resp = ctr.newGame("_buildable", GameMode.PEACEFUL.getValue());
+        DungeonResponse resp = ctr.newGame("_buildable_sceptre", GameMode.PEACEFUL.getValue());
         assertThrows(InvalidActionException.class, () -> {
-            ctr.build(Shield.STRING_TYPE);
+            ctr.build(Sceptre.STRING_TYPE);
         });
-        resp = ctr.tick(null, Direction.RIGHT); // pick up the wood
-        assertThrows(InvalidActionException.class, () -> {
-            ctr.build(Shield.STRING_TYPE);
-        });
-        resp = ctr.tick(null, Direction.RIGHT); // pick up the second wood
-        assertThrows(InvalidActionException.class, () -> {
-            ctr.build(Shield.STRING_TYPE);
-        });
-        resp = ctr.tick(null, Direction.RIGHT); // pick up the treasure
-        List<String> actual = resp.getInventory().stream().map(ir -> ir.getType()).collect(Collectors.toList());
-        List<String> expected = Arrays.asList(Wood.STRING_TYPE, Wood.STRING_TYPE, Treasure.STRING_TYPE);
+
+        // pick up the materials
+        resp = ctr.tick(null, Direction.LEFT); 
+        resp = ctr.tick(null, Direction.UP); 
+        resp = ctr.tick(null, Direction.DOWN); 
+        resp = ctr.tick(null, Direction.RIGHT); 
+        resp = ctr.tick(null, Direction.DOWN); 
+
+        List<String> actual = resp.getInventory().stream().map(item -> item.getType()).collect(Collectors.toList());
+        List<String> expected = Arrays.asList(Wood.STRING_TYPE, Key.STRING_TYPE, SunStone.STRING_TYPE);
         Collections.sort(actual);
         Collections.sort(expected);
         assertEquals(expected, actual);
-
         actual.clear();
+
         assertDoesNotThrow(() -> {
-            DungeonResponse r = ctr.build(Shield.STRING_TYPE);
+            DungeonResponse r = ctr.build(Sceptre.STRING_TYPE);
             actual.addAll(r.getInventory().stream().map(ir -> ir.getType()).collect(Collectors.toList()));
         });
-        assertEquals(List.of(Shield.STRING_TYPE), actual);
+        assertEquals(List.of(Sceptre.STRING_TYPE), actual);
     }
 }
