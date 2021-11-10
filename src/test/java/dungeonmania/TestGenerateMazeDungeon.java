5e0745a0b361919ee4c4907c2ae07ce21b9eb5fb
Mathieu PATUREL <australie.p@gmail.com>
2021-11-10 13:07:40 +1100

    generate dungeon: ensure player can move in at least one direction

--- src/test/java/dungeonmania/TestGenerateMazeDungeon.java
+++ src/test/java/dungeonmania/TestGenerateMazeDungeon.java
@@ -1,12 +1,17 @@
 package dungeonmania;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 
 import dungeonmania.DungeonManiaController.GameMode;
 import dungeonmania.entities.statics.Exit;
 import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.util.Direction;
 import dungeonmania.util.Position;
 
 public class TestGenerateMazeDungeon {
@@ -24,6 +29,15 @@ public class TestGenerateMazeDungeon {
             .filter(e -> e.getType().equals(Exit.STRING_TYPE)).findFirst().get().getPosition();
         assertEquals(47, exitPosition.getX());
         assertEquals(45, exitPosition.getY());
+
+        // ensure that we can move in one direction at least
+        Set<Pos2d> positions = new HashSet<>();
+        for (Direction d : Direction.values()) {
+            resp = ctr.tick(null, d);
+            p = TestUtils.getPlayerPosition(resp);
+            positions.add(new Pos2d(p.getX(), p.getY()));
+        }
+        assertTrue(positions.size() <= 1, "the player wasn't able to move in any direction");
     }
     
 }
