65418f18b938f46432aecedbbd762dc1d8ea85f2
CarelGreylin <z5235774@ad.unsw.edu.au>
2021-11-10 17:26:09 +1100

    Tests for pathfinding with costs.

--- src/test/java/dungeonmania/TestMovement.java
+++ src/test/java/dungeonmania/TestMovement.java
@@ -1,15 +1,21 @@
 package dungeonmania;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import dungeonmania.DungeonManiaController.GameMode;
+import dungeonmania.entities.movings.Assassin;
+import dungeonmania.entities.movings.Mercenary;
 import dungeonmania.goal.ExitGoal;
 import dungeonmania.movement.CircleMovementBehaviour;
 import dungeonmania.movement.FleeMovementBehaviour;
@@ -18,6 +24,7 @@ import dungeonmania.movement.FriendlyMovementBehaviour;
 import dungeonmania.movement.MovementBehaviour;
 import dungeonmania.movement.RandomMovementBehaviour;
 import dungeonmania.util.Counter;
+import dungeonmania.util.Direction;
 import dungeonmania.util.FileLoader;
 
 public class TestMovement {
@@ -31,6 +38,122 @@ public class TestMovement {
         dungeon = Dungeon.fromJSONObject("name", GameMode.STANDARD, new JSONObject(content));
         dungeon.getMap().flood();
         startingCell = dungeon.getMap().getCell(new Pos2d(2, 2));
+    }
+
+    @Test
+    public void testPathFinding1() throws IOException 
+    {
+        String content = FileLoader.loadResourceFile("/dungeons/_pathfinding1.json");
+        dungeon = Dungeon.fromJSONObject("name", GameMode.STANDARD, new JSONObject(content));
+        DungeonManiaController dc = new DungeonManiaController(dungeon);
+        Mercenary assassin = TestUtils.getMercenary(dungeon);
+
+        List<Pos2d> followedPath = new ArrayList<>();
+
+        // let the assassin find it's path
+        while (!assassin.getPosition().equals(new Pos2d(0, 0))) {
+            dc.tick(null, Direction.NONE);
+            if (!followedPath.stream().anyMatch(p -> p.equals(assassin.getPosition())))
+                followedPath.add(assassin.getPosition());
+        }
+
+        // define expected path
+        List<Pos2d> expectedPath = Arrays.asList(
+            new Pos2d(0, 0),
+            new Pos2d(0, 1)
+        );
+
+        assertEquals(expectedPath.size(), followedPath.size());
+        for (int i = 0; i < followedPath.size(); i++) {
+            assertEquals(expectedPath.get(i), followedPath.get(i));
+        }
+    }
+
+    @Test
+    public void testPathFinding2() throws IOException 
+    {
+        String content = FileLoader.loadResourceFile("/dungeons/_pathfinding2.json");
+        dungeon = Dungeon.fromJSONObject("name", GameMode.STANDARD, new JSONObject(content));
+        DungeonManiaController dc = new DungeonManiaController(dungeon);
+        Mercenary assassin = TestUtils.getMercenary(dungeon);
+
+        List<Pos2d> followedPath = new ArrayList<>();
+
+        // let the assassin find it's path
+        while (!assassin.getPosition().equals(new Pos2d(0, 0))) {
+            dc.tick(null, Direction.NONE);
+            if (!followedPath.stream().anyMatch(p -> p.equals(assassin.getPosition())))
+                followedPath.add(assassin.getPosition());
+        }
+
+        // define expected path
+        List<Pos2d> expectedPath = Arrays.asList(
+            new Pos2d(0, 0),
+            new Pos2d(0, 1)
+        );
+
+        assertEquals(expectedPath.size(), followedPath.size());
+        for (int i = 0; i < followedPath.size(); i++) {
+            assertEquals(expectedPath.get(i), followedPath.get(i));
+        }
+    }
+
+    @Test
+    public void testPathFinding3() throws IOException 
+    {
+        String content = FileLoader.loadResourceFile("/dungeons/_pathfinding3.json");
+        dungeon = Dungeon.fromJSONObject("name", GameMode.STANDARD, new JSONObject(content));
+        DungeonManiaController dc = new DungeonManiaController(dungeon);
+        Mercenary assassin = TestUtils.getMercenary(dungeon);
+
+        List<Pos2d> followedPath = new ArrayList<>();
+
+        // let the assassin find it's path
+        while (!assassin.getPosition().equals(new Pos2d(0, 0))) {
+            dc.tick(null, Direction.NONE);
+            if (!followedPath.stream().anyMatch(p -> p.equals(assassin.getPosition())))
+                followedPath.add(assassin.getPosition());
+        }
+
+        // define expected path
+        List<Pos2d> expectedPath = Arrays.asList(
+            new Pos2d(0, 0),
+            new Pos2d(0, 1)
+        );
+
+        assertEquals(expectedPath.size(), followedPath.size());
+        for (int i = 0; i < followedPath.size(); i++) {
+            assertEquals(expectedPath.get(i), followedPath.get(i));
+        }
+    }
+
+    @Test
+    public void testPathFinding4() throws IOException 
+    {
+        String content = FileLoader.loadResourceFile("/dungeons/_pathfinding4.json");
+        dungeon = Dungeon.fromJSONObject("name", GameMode.STANDARD, new JSONObject(content));
+        DungeonManiaController dc = new DungeonManiaController(dungeon);
+        Mercenary assassin = TestUtils.getMercenary(dungeon);
+
+        List<Pos2d> followedPath = new ArrayList<>();
+
+        // let the assassin find it's path
+        while (!assassin.getPosition().equals(new Pos2d(0, 0))) {
+            dc.tick(null, Direction.NONE);
+            if (!followedPath.stream().anyMatch(p -> p.equals(assassin.getPosition())))
+                followedPath.add(assassin.getPosition());
+        }
+
+        // define expected path
+        List<Pos2d> expectedPath = Arrays.asList(
+            new Pos2d(0, 0),
+            new Pos2d(0, 1)
+        );
+
+        assertEquals(expectedPath.size(), followedPath.size());
+        for (int i = 0; i < followedPath.size(); i++) {
+            assertEquals(expectedPath.get(i), followedPath.get(i));
+        }
     }
 
     @Test
