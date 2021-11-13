308f56ce17c43121f5bfece36861c2883de0df04
jgonzaga35 <justingonzaga35@gmail.com>
2021-11-14 02:40:15 +1100

    added testing file with no tests

---
+++ src/test/java/dungeonmania/TestLogicSwitch.java
@@ -0,0 +1,17 @@
+package dungeonmania;
+
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+// import dungeonmania.DungeonManiaController.GameMode;
+// import dungeonmania.response.models.DungeonResponse;
+// import dungeonmania.util.Direction;
+// import dungeonmania.util.Position;
+
+public class TestLogicSwitch {
+
+// test and, or, xor, not, co_and
+// test complex logic gates    
+
+}
